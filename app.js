498d37f519724f4a902f4f99a026381c7c9090a3
CyberDack <afntwrk.guyrpg@gmail.com>
2021-07-10 16:30:53 -0600

    made better coin buttons

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const coins = {
       coinRate: 0.00010000,
       marketValue: 100,
       marketRate: 10,
-      img: 'BTC.png'
+      img: 'BTC.png',
+      rigged: true,
    },
    DOGE: {
       name: 'DOGE',
@@ -14,6 +15,7 @@ const coins = {
       marketValue: 1.00,
       marketRate: 1,
       img: 'DOGE.png',
+      rigged: false,
 
    },
    ETH: {
@@ -23,6 +25,7 @@ const coins = {
       marketValue: 75,
       marketRate: 8,
       img: 'ETH.png',
+      rigged: false,
 
    }
 }
@@ -177,6 +180,9 @@ function drawCount() {
       coins[key].owned = Number(coins[key].owned.toFixed(8))
       let countStr = coins[key].owned.toFixed(8)
       document.getElementById(`${key}-count`).innerText = `${key}: ${countStr}`
+      let coinVal = coins[key].owned * coins[key].marketValue
+      let coinValStr = coinVal.toFixed(2)
+      document.getElementById(`${key}-coinval`).innerText = `$${coinValStr}`
    }
    USD = Number(USD.toFixed(2))
    document.getElementById('usd-count').innerText = `USD: $${USD}`
@@ -219,11 +225,29 @@ function drawMarket() {
 function drawCoins() {
    let template = ''
    for (let key in coins) {
+      let coinVal = coins[key].owned * coins[key].marketValue
+      coinVal = coinVal.toFixed(2)
+      let riggedOn = ''
+      if (coins[key].rigged) {
+         riggedOn = `<h4 class="col-3">RIG</h4>`
+      } else {
+         riggedOn = `<button class="col-3 btn btn-primary">MINE</button>`
+      }
       template +=
-         `<div class="col-4 text-center">
-            <img class="btn" onclick="clickMine('${key}')" src="${coins[key].img}"></img>
-            <h5>Mining Rate: ${coins[key].coinRate} per click.</h5>
-         </div>`
+         `<div class="col-4" id="${coins[key].name}-card">
+         <div class="row justify-content-center text-center">
+            <img class="btn col-12" onclick="clickMine('${key}')" src="${coins[key].img}"></img>
+            <h5 class="col-12">Mining Rate: ${coins[key].coinRate}0 per click.</h5>
+            <h3 class="col-12" id="${key}-coinval">${coinVal}</h3>
+            <h4 class="col-9" id="${key}-count">${coins[key].owned}</h4>
+            ${riggedOn}
+            <button class="btn btn-primary col-3" onclick="sellCoin('${key}',.10)">10%</button>
+            <button class="btn btn-primary col-3" onclick="sellCoin('${key}',.25)">25%</button>
+            <button class="btn btn-primary col-3" onclick="sellCoin('${key}',.50)">50%</button>
+            <button class="btn btn-primary col-3" onclick="sellCoin('${key}', 1)">ALL</button>
+         </div>
+      </div>`
+
    }
    document.getElementById('coins').innerHTML = template
 }
